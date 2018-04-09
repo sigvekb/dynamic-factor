76b49627c4e0ba6378f7f8bfa15b8f633998af64
sigvekb <sigvekb@gmail.com>
2018-04-09 17:10:05 +0200

    Clean up code, remove unused/unnecessary elements

--- DynamicFactorModel.m
+++ DynamicFactorModel.m
@@ -1,4 +1,4 @@
-function [x,F_hat,F_pc,F_kal,num_iter, C, A, Q] = DynamicFactorModel(X,q,r,p,max_iter, thresh, block)
+function [x,F_hat,F_pc,num_iter, C, A, Q] = DynamicFactorModel(X,q,r,p,max_iter, thresh, block)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % "A Quasi?Maximum Likelihood Approach for Large, Approximate Dynamic Factor Models," 
 % The Review of Economics and Statistics, MIT Press, vol. 94(4), pages 1014-1024, November 2012.
@@ -39,12 +39,11 @@ function [x,F_hat,F_pc,F_kal,num_iter, C, A, Q] = DynamicFactorModel(X,q,r,p,max
 OPTS.disp = 0;
 [T,N] = size(X);
 
-Mx = mean(X);
-Wx = (std(X));
-% x is the matrix of standardized observable variables
-x = (X-kron(ones(T,1),Mx))*diag(1./Wx);
+demean = bsxfun(@minus, X, nanmean(X));
+x = bsxfun(@rdivide, demean, nanstd(X));
 
-% the number of static factors cannot be great of the dynamic ones
+
+% the number of static factors cannot be greater than dynamic
 if r < q
     error('q has to be less or equal to r')
 end
@@ -52,7 +51,7 @@ end
 nlag = p-1;
 
 A_temp = zeros(r,r*(nlag + 1))';
-I = eye(r*(nlag+1),r*(nlag+1));
+I = eye(r*(nlag+1),r*(nlag + 1));
 A = [A_temp';I(1:end-r,1:end)];
 
 Q = zeros((nlag+1)*r,(nlag+1)*r);
@@ -60,12 +59,17 @@ Q(1:r,1:r) = eye(r);
 
 OPTS.disp=0;
 
-%extract the first r eigenvectors and eigenvalues from cov(x)
-[ v, ~ ] = eigs(cov(x),r,'lm',OPTS);
+% Create pseudo dataset, replacing NaN with 0, in order to make
+% the initialization good
+pseudo_x = x;
+pseudo_x(isnan(pseudo_x)) = 0;
 
-chi = x*(v*v');                       % Common component
+% Extract the first r eigenvectors and eigenvalues from cov(x)
+[ v, ~ ] = eigs(cov(pseudo_x),r,'lm',OPTS);
 
-dES = eye(r);
+chi = pseudo_x*(v*v');                       % Common component
+
+d = eye(r);
 
 F = x*v;
 
@@ -97,12 +101,9 @@ for kk = 0:nlag
     Z = [Z z(nlag-kk+1:end-kk,:)];  % stacked regressors (lagged SPC)
 end
 
-initx = Z(1,:)';                    %initial state mean                                                            
-
-
-% initial state covariance
-initV = reshape(pinv(eye((r*nlag+1))^2-kron(A,A))*Q(:),r*(nlag+1),r*(nlag+1));eye(r*(nlag+1));
-
+% Initial factor values, initial state covariance
+initx = Z(1,:)';                                                                                
+initV = reshape(pinv(eye((r*nlag+1))^2-kron(A,A))*Q(:),r*(nlag+1),r*(nlag+1));
 
 C = [v zeros(N,r*(nlag))];
 
@@ -127,52 +128,25 @@ end
 % initialize the estimation and define ueful quantities
 
 previous_loglik = -inf;
-
-loglik = [];
 num_iter = 1;
 LL = zeros(1, max_iter);
+converged = 0;
 
 os = size(C,1);     % number of cross sections ( N )
 ss = size(A,1);     % number of factors ( r )
 y = x';
 
-
-
-converged = 0;
-
-% estimation of the factors with the Kalman filter using as initial values
-% for A, C, Q, R, initx, initV the ones computed with the principal
-% components
-[xitt,xittm,Ptt,Pttm,loglik_t]=KalmanFilter(initx,initV,x,A,C,R,Q);
-[xsmooth, Vsmooth, VVsmooth]=KalmanSmoother(A,xitt,xittm,Ptt,Pttm,C,R);
-
-F_kal =  xsmooth(1:r,:)';
-
-
-
-%factors estimation with the EM algorithm
-%repeat the algorithm until convergence
+% Estimation with the EM algorithm
+% Repeat the algorithm until convergence
 while (num_iter < max_iter) && ~converged
     
-    %%% E step : compute the sufficient statistics 
-    
-    % In our model the sufficient statistics are
-    % delta = sum_t=1^T (x_t * f'_t)
-    % gamma = sum_t=1^T (f_t * f'_t)   
-    % gamma1 = sum_t=2^T (f_t-1 * f'_t-1)
-    % gamma2 = sum_t=2^T (f_t * f'_t)
-    % beta = sum_t=1^T (f_t * f'_t-1)
-    % P1sum  variance of the initial state
-    % x1sum  expected value of the initial state 
-    
-    % use the function Estep to update the expected sufficient statistics
-    % note that at the first iteration  we use as initial values for A, C, Q, 
-    % R, initx, initV the ones computed with the principal components
+    % E step : computes the expected sufficient statistics 
+    % First iteration uses initial values for A, C, Q, R, initx, initV 
+    % obtained from the principal component analysis
     [beta_t_AQ, gamma_t_C, gamma_t_AQ, delta_t_C, gamma1_t_AQ, gamma2_t_AQ, x1, V1, loglik_t,xsmooth] = ...
         Estep(y, A, C, Q, R, initx, initV, block);
     
-    % fix the expected sufficient statistics equal to the one computed with
-    % the function Estep
+    % Hand over the expected sufficient statistics from E-step
     beta_AQ = beta_t_AQ;        % beta   = sum_t=1^T (f_t * f'_t-1)
     gamma_C = gamma_t_C;        % gamma  = sum_t=1^T (f_t * f'_t)   
     gamma_AQ = gamma_t_AQ;      % gamma  = sum_t=1^T (f_t * f'_t)
@@ -181,23 +155,20 @@ while (num_iter < max_iter) && ~converged
     gamma2_AQ = gamma2_t_AQ;    % gamma2 = sum_t=2^T (f_t * f'_t)
     P1sum = V1 + x1*x1';        % P1sum    variance of the initial state
     x1sum = x1;                 % x1sum    expected value of the initial state
-    
                                                                             
-    % update the loglikelihood                                                                           
+    % Update the log likelihood                                                                           
     loglik = loglik_t;
     LL(num_iter) = loglik;
     
     %%% M step 
-    % compute the parameters of the model as a function of the sufficient
-    % statistics (computed with the function Estep)
+    % Compute the parameters of the model as a function of the sufficient
+    % statistics
     
-    % The formulas for the parameters derive from the maximum likelihood
-    % method. In the EM algorithm we substitute in the ML estimator the 
-    % sufficient statistics (computed in the E step) and then iterate the 
-    % procedure till the maximization of the likelihood
+    % The parameters are found through maximum likelihood optimisation
+    % In the EM algorithm we substitute the sufficient statistics 
+    % calculated earlier.
     
     % C = (sum_t=1^T x_t*f'_t)* (sum_t=1^T f_t*f'_t)^-1 
-    % substituting for the sufficient statistics
     prev_split = 1;
     split = 1;
     for i=1:length(block)
@@ -259,24 +230,18 @@ end
 [xitt,xittm,Ptt,Pttm,loglik_t]=KalmanFilter(initx,initV,x,A,C,R,Q);
 [xsmooth, Vsmooth, VVsmooth]=KalmanSmoother(A,xitt,xittm,Ptt,Pttm,C,R);
 
-chi = xsmooth'*C'*diag(Wx) + kron(ones(T,1),Mx);
-
 F_hat =  xsmooth(1:r,:)';
 
 
 function [converged, decrease] = em_converged(loglik, previous_loglik, threshold, check_increased)
 % EM_CONVERGED Has EM converged?
-% [converged, decrease] = em_converged(loglik, previous_loglik, threshold)
-%
-% We have converged if the slope of the log-likelihood function falls below 'threshold', 
+% The EM optimisation converges if the slope of the log-likelihood 
+% function falls below the set threshold, 
 % i.e., |f(t) - f(t-1)| / avg < threshold,
 % where avg = (|f(t)| + |f(t-1)|)/2 and f(t) is log lik at iteration t.
 % 'threshold' defaults to 1e-4.
 %
 % This stopping criterion is from Numerical Recipes in C p423
-%
-% If we are doing MAP estimation (using priors), the likelihood can decrase,
-% even though the mode of the posterior is increasing.
 
 if nargin < 3, threshold = 1e-4; end
 if nargin < 4, check_increased = 1; end
