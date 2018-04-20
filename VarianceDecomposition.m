1914657ef250afcc560e2c7ff13b6ec1326fb02c
sigvekb <sigvekb@gmail.com>
2018-04-20 18:35:31 +0200

    Update calculation of variance decomposition

--- VarianceDecomposition.m
+++ VarianceDecomposition.m
@@ -1,26 +1,16 @@
 function [varDecomp] = VarianceDecomposition(normData, factors, loadings)
 [n,r] = size(loadings);
-varDecomp = zeros(n,4);
+varDecomp = zeros(n,r+2);
 
-i_fact = normData;
-
-all = cell(1, r);
+% Find variance explained for each factor
 for f=1:r
-    all{f} = factors(:,f)*loadings(:,f)';
+    factorVar = var(factors(:,f));
+    varDecomp(:,f) = loadings(:,f).^2 * factorVar;
 end
 
-for v=1:n
-    for f=1:r
-        if f==1
-            varDecomp(v,1) = var(all{f}(:,v));
-            i_fact(:,v) = i_fact(:,v) - all{f}(:,v);
-        else
-            if sum(all{f}(:,v))~=0
-                varDecomp(v,2) = var(all{f}(:,v));
-                i_fact(:,v) = i_fact(:,v) - all{f}(:,v);
-            end
-        end
-    end
-    varDecomp(v,3) = nanvar(i_fact(:,v));
-    varDecomp(v,4) = sum(varDecomp(v,1:3));
-end
+% Sum over variances of block factors
+varDecomp(:,r+1) = sum(varDecomp(:,2:r),2);
+
+% Find error term variance
+idio = normData - factors*loadings';
+varDecomp(:,r+2) = nanvar(idio)';
